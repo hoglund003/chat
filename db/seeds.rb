f41906de6e13428acf15db1f019550b111484665
kasper_hoglund <kasper@rubynor.com>
2024-01-08 15:56:19 +0100

    Add Channel model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 User.destroy_all
-
+Channel.destroy_all
 
 30.times do
   nickname = Faker::Internet.username
@@ -10,5 +10,15 @@ User.destroy_all
     password: "password",
     password_confirmation: "password",
     )
-  puts "User created!" if user.save
+  puts "User created!" if user.save!
+end
+
+channel_names = %w[general specific announcements news random]
+
+channel_names.each do |channel_name|
+  channel = Channel.new(
+    name: channel_name
+  )
+
+  puts "Channel created!" if channel.save!
 end
