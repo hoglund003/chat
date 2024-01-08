6afe41b1c94515cf8bb0eb68a1dc5bf99b8bab74
kasper_hoglund <kasper@rubynor.com>
2024-01-08 18:18:18 +0100

    Add new channel from

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  resources :channels, only: [:index, :new, :create]
+
+  root "channels#index"
 end
