ba030ecda441c27c3c7eeef69572e8c69a44963a
kasper_hoglund <kasper@rubynor.com>
2024-01-09 16:17:26 +0100

    Update theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,28 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: "#3990d2",
+        "primary-content": "#ffffff",
+        "primary-dark": "#2875b0",
+        "primary-light": "#63a7db",
+        secondary: "#d24839",
+        "secondary-content": "#ffffff",
+        "secondary-dark": "#b03528",
+        "secondary-light": "#db6e63",
+        background: "#eef0f2",
+        foreground: "#fbfbfc",
+        border: "#dbe0e3",
+        copy: "#21272b",
+        "copy-light": "#596873",
+        "copy-lighter": "#7e8e9b",
+        success: "#39d239",
+        warning: "#d2d239",
+        error: "#d23939",
+        "success-content": "#020a02",
+        "warning-content": "#0a0a02",
+        "error-content": "#ffffff"
+      },
     },
   },
   plugins: [
@@ -20,5 +42,16 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
     require("daisyui"),
-  ]
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "blue",
+          secondary: "teal",
+        },
+      },
+    ],
+  },
 }
