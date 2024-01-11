b7b723dcce6b3423fdd311c9f99d205f29c9ec34
kasper_hoglund <kasper@rubynor.com>
2024-01-11 21:43:48 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 Message.destroy_all
 User.destroy_all
 Channel.destroy_all
+UnreadNotification.destroy_all
 
 30.times do
   nickname = Faker::Internet.username
@@ -26,12 +27,18 @@ end
 Channel.all.each do |channel|
   100.times do
     date = rand((4.months.ago)..(0.days.ago))
+    user = User.all.sample
     message = Message.new(
       text: Faker::Lorem.sentence(word_count: 3),
       channel: channel,
-      user: User.all.sample,
+      user: user,
       created_at: date,
     )
     puts "Message created!" if message.save!
+
+    User.where.not(id: user.id).each do |user|
+      notification = UnreadNotification.new(user: user, message: message)
+      puts "Notification created!" if notification.save!
+    end
   end
 end
