e1395eaf125b1817ff81d96e4e62094c13bb5557
kasper_hoglund <kasper@rubynor.com>
2024-01-12 08:12:57 +0100

    Fix test

--- spec/helpers/message_helper_spec.rb
+++ spec/helpers/message_helper_spec.rb
@@ -12,8 +12,8 @@ require 'rails_helper'
 # end
 RSpec.describe MessageHelper, type: :helper do
   describe "value" do
-    let!(:message1) { build(:message, created_at: DateTime.parse("2024-01-10T12:23")) }
-    let!(:message2) { build(:message, created_at: DateTime.parse("2024-04-26T05:58")) }
+    let!(:message1) { build(:message, created_at: Time.zone.parse("2024-01-10T12:23")) }
+    let!(:message2) { build(:message, created_at: Time.zone.parse("2024-04-26T05:58")) }
     it 'should show date and time' do
       expect(sent_at(message1)).to eq("12:23 10/01")
       expect(sent_at(message2)).to eq("05:58 26/04")
